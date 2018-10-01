f6733938006cab5370d9d6cb36ba99662336fe55
Patrick Buckingham <patrick@hypergiant.com>
2018-10-01 11:00:01 -0500

    Updated Redux tutorial

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,4 +1,3 @@
 import { KILL_WASP, KILL_MANY_WASPS } from '../constants/action-types.js';
 
 export const killWasp = () => ({type: KILL_WASP});
-export const killManyWasps = (count)=>({type: KILL_MANY_WASPS, payload: count});
